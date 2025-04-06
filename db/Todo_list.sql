30e1b25444089c7fb2b6514e631502ec252aa6c0
HongyiHao-SXIT <Lanyi_adict@outlook.com>
2025-04-06 22:14:39 +0800

    change the item

--- db/Todo_list.sql
+++ db/Todo_list.sql
@@ -1,4 +1,4 @@
--- Active: 1736786939860@@127.0.0.1@3306@furry_bloggie
+-- Active: 1736786939860@@127.0.0.1@3306@todo_list
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
